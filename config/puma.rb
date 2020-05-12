a65ba3d25dcfd1489f034be31e1273c6c21371f9
mkoki <matsukoutennis@gmail.com>
2020-05-12 11:41:52 +0900

    remove ruckup

--- config/puma.rb
+++ config/puma.rb
@@ -21,7 +21,7 @@ threads threads_count, threads_count
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 preload_app!
-rackup　DefaultRackup
+# rackup　DefaultRackup
 port        ENV['PORT'] || 3000
 environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
