26012eb49fdcc433978e5dfd5bfff8dcc442889c
mkoki <matsukoutennis@gmail.com>
2020-05-12 11:21:30 +0900

    puma fix

--- config/puma.rb
+++ config/puma.rb
@@ -4,15 +4,30 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "production" }
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+# max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+# min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+# threads min_threads_count, max_threads_count
+# port        ENV.fetch("PORT") { 3000 }
+# environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "production" }
+# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+# preload_app!
+# plugin :tmp_restart
+
+
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+threads threads_count, threads_count
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
 preload_app!
-plugin :tmp_restart
+rackup　DefaultRackup
+port        ENV['PORT'] || 3000
+environment ENV['RACK_ENV'] || 'development'
+on_worker_boot do
+#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+  ActiveRecord::Base.establish_connection
+end
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
