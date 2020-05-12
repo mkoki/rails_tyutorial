0eb033a6f7b8bad47cd287d7d98c7085c7016aab
mkoki <matsukoutennis@gmail.com>
2020-05-12 13:38:41 +0900

    local

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,10 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # アップロードされたファイルをAWSに保存する
-  config.active_storage.service = :amazon
+
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :local
+
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
